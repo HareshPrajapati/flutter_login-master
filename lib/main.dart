b5f1403e23f2f3a148555e6ab578b874709b21b8
Haresh Prajapati <sondagarharesh@gmail.com>
2021-04-27 12:32:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/services/authentication.dart';
+import 'package:flutter_login/pages/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // login page parameters:
+  // primary swatch color
+  static const primarySwatch = Colors.green;
+  // button color
+  static const buttonColor = Colors.green;
+  // app name
+  static const appName = 'Temprature Chart';
+  // boolean for showing home page if user unverified
+  static const homePageUnverified = false;
+
+  final params = {
+    'appName': appName,
+    'primarySwatch': primarySwatch,
+    'buttonColor': buttonColor,
+    'homePageUnverified': homePageUnverified,
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Temprature chart',
+        debugShowCheckedModeBanner: true,
+        theme: new ThemeData(
+          primarySwatch: params['primarySwatch'],
+        ),
+        home: new RootPage(params: params, auth: new Auth()));
+  }
+}
